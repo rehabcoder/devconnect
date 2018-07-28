cc3f780c485800fc4a8c66e3194b30113ae55d9c
unknown <akshay.saini@eaglecrk.com>
2018-07-28 18:28:40 -0400

    User API Routes & JWT Authentication

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: 'mongodb://preet:admin1@ds147361.mlab.com:47361/devconnect'
+  mongoURI: 'mongodb://preet:admin1@ds147361.mlab.com:47361/devconnect',
+  secretOrKey: 'secret'
 };
